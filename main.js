b01f388db80445a562440bf447add141f0638cec
NistorA2 <Anca.Nistor@paddypowerbetfair.com>
2019-05-15 19:24:53 +0300

    Add GET method after products

---
+++ main.js
@@ -0,0 +1,15 @@
+//define API url
+var apiUrl = "http://alpha.apexcode.ro/api";
+
+//metoda de GET prin Fetch API
+    fetch(apiUrl + '/products', {
+        method: 'GET'
+    }).then(function(response) {
+        return response.json();
+    }).then(function(datele){
+        for(var i=0; i< datele.length; i++){
+            // JSON.stringify - converteste un JSON in string
+            document.getElementById("container").innerHTML +=
+            "<li>"+ JSON.stringify(datele[i]) + "</li>";
+    }
+});
