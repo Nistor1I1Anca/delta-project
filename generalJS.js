6b5961bcfe446711dc31f4ea52ac166c02355b5f
Alex <atfrancu@gmail.com>
2019-05-29 20:58:25 +0300

    changes

---
+++ generalJS.js
@@ -0,0 +1,11 @@
+function setWelcome() {
+  console.log(1235);
+  document.getElementById(
+    "welcome"
+  ).innerHTML = `<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Invoice generator</h1>
+      <p class="lead">The only invoice generator u'll ever need to use</p>
+    </div>
+  </div>`;
+}
